bcc6e716cf42f7c0bfd4781a91868521a75ec028
EDGI9 <EDGI9@users.noreply.github.com>
2022-09-30 15:46:41 +0000

    deploy: 3b0735e9ce8da079ad14bfbef3321b5cf87f7601

--- src/api/modules/pokemons.js
+++
@@ -1,5 +0,0 @@
-export default {
-  getAllPokemons: "https://pokeapi.co/api/v2/pokemon",
-  getPokemon: "https://pokeapi.co/api/v2/pokemon/{pokemonName}",
-  getPokemonImage: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{number}.png"
-};
